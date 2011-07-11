49635cc67604dfa7b14fab88f05685239f73cefa
Craig Blaszczyk <masterjakul@gmail.com>
2011-07-11 15:19:05 +0100

    update functionality and tests

--- timeofweek/tests.py
+++ timeofweek/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from timeofweek.util import TimeOfWeek
+from timeofweek.util import TimeOfWeek, InvalidTimeOfWeekException,\
+    InvalidDayException, InvalidTimeException, InvalidPeriodException
 
 
 class TimeOfWeekTest(TestCase):
@@ -11,13 +12,90 @@ class TimeOfWeekTest(TestCase):
         self.times = ('0000', '0920', '1200', '1545', '2359')
 
         self.invalid_days = ('M', 'T', 'monday', 'ananan')
-        self.invalid_times = ('12345', '', '1', '123', '12', None, '-1')
+        self.invalid_days2 = ('ABC', 'MOO')
+        self.invalid_times = ('12345', '', '1', '123', '12', '-1', '1.2')
+        self.invalid_times2 = ('3000', '9999')
         
-    def test_basic(self):
-        #this represents all times in the week
+        
+        
+    def test_tow(self):
+        """
+        Test the parsing of dates/time from a number of strings
+        Test the parsing of periods
+        Test the storage of periods
+        """
         tow = TimeOfWeek()
+        
+        self.assertFalse(None in tow)
         
         for day in self.day_names:
             for time in self.times:
                 self.assertTrue(day + time in tow)
+            for time in self.invalid_times:
+                self.assertRaises(
+                    InvalidTimeOfWeekException, tow.__contains__, day + time
+                )          
+            for time in self.invalid_times2:
+                self.assertRaises(
+                    InvalidTimeException, tow.__contains__, day + time
+                )                                
+            
+        for time in self.times:    
+            for day in self.invalid_days:
+                self.assertRaises(
+                    InvalidTimeOfWeekException, tow.__contains__, day + time
+                )            
+            for day in self.invalid_days2:
+                self.assertRaises(
+                    InvalidDayException, tow.__contains__, day + time
+                )
+                
+        self.assertRaises(InvalidPeriodException, TimeOfWeek, '')
+        self.assertRaises(InvalidPeriodException, TimeOfWeek, 'MON 1200-1700')
+        self.assertRaises(InvalidPeriodException, TimeOfWeek, 'MON1200-1700, ')
+        self.assertRaises(InvalidPeriodException, TimeOfWeek, 'MON1200-1700, TUE1000-1000')
+        
+        period = 'MON1200-1700,TUE1200-2359,HOL1000-1200'
+        tow = TimeOfWeek(period)
+        self.assertTrue('MON1200' in tow)
+        self.assertTrue('TUE2358' in tow)
+        self.assertTrue('HOL1200' in tow)
+        self.assertFalse('MON1159' in tow)
+        self.assertFalse('HOL0900' in tow)
+        self.assertFalse('HOL1201' in tow)
+        self.assertFalse('WED1201' in tow)
+        self.assertFalse('THU1201' in tow)
+        
+        period = 'MON1200-1200'
+        tow = TimeOfWeek(period)
+        self.assertTrue('MON1200' in tow)
+        self.assertFalse('MON1159' in tow)
+        self.assertFalse('MON1201' in tow)
+        
+        tow = TimeOfWeek('MON1200-1300') + TimeOfWeek('TUE1400-1500')
+        self.assertTrue('MON1200' in tow)
+        self.assertTrue('TUE1400' in tow)
+        
+        #ensure adding works symmetrically
+        tow = TimeOfWeek('TUE1400-1500') + TimeOfWeek('MON1200-1300')
+        self.assertTrue('MON1200' in tow)
+        self.assertTrue('TUE1400' in tow)
+        
+        tow = TimeOfWeek('MON1200-1300') + TimeOfWeek('MON0900-1500')
+        self.assertTrue('MON1200' in tow)
+        self.assertTrue('MON1159' in tow)
+        self.assertFalse('MON0859' in tow)
+        self.assertFalse('MON1501' in tow)
+        
+        tow = TimeOfWeek('MON1200-1700') + TimeOfWeek('MON0900-1300')
+        self.assertTrue('MON1200' in tow)
+        self.assertTrue('MON1159' in tow)
+        self.assertTrue('MON1301' in tow)
+        self.assertTrue('MON1700' in tow)
+        self.assertFalse('MON0859' in tow)
+        self.assertFalse('MON1701' in tow)
+                        
+        
+        
+                   
         
--- timeofweek/util.py
+++ timeofweek/util.py
@@ -1,4 +1,20 @@
+class TimeOfWeekException(Exception):
+    pass
 
+class InvalidTimeOfWeekException(TimeOfWeekException):
+    pass
+
+class InvalidDayException(TimeOfWeekException):
+    pass
+
+class InvalidTimeException(TimeOfWeekException):
+    pass
+
+class InvalidPeriodException(TimeOfWeekException):
+    pass
+
+class InvalidTimePeriodException(TimeOfWeekException):
+    pass
 
 class TimeOfWeek(object):
     """
@@ -12,13 +28,117 @@ class TimeOfWeek(object):
         Converts a string representing a time of week into a TimeOfWeek object 
         """
         
-        self._days = {}
-        for day_name in self._day_names:
-            self._days[day_name] = [0000, 2359]
-           
-        if tow is not None: 
+        self._periods = {}
+        if tow is None:
+            for day_name in self._day_names:
+                self._periods[day_name] = [0000, 2359]
+        else:
             periods = tow.split(',')
             for period in periods:
-                start, stop = period.split('-')
+                day_name, start_time, end_time = self.__parse_period(period)
+                self._periods[day_name] = (start_time, end_time)
+                
+                
+    def __contains__(self, key):
+        if key is None:
+            return False
+        
+        day, time = self.__parse_time(key)
+        
+        if not self._periods.has_key(day):
+            #not valid at anytime on this day
+            return False
+        
+        start_time, end_time = self._periods.get(day)
+        if time < start_time or time > end_time:
+            #time is outside allowed bounds
+            return False
+        
+        return True
+    
+    def __parse_time(self, tow):
+        if len(tow) != 7:
+            raise InvalidTimeOfWeekException(tow)
+        
+        day = tow[0:3]
+        if day not in self._day_names:
+            raise InvalidDayException(day)
+    
+        time = tow[3:]
+        try:
+            time = int(time)
+        except (ValueError,):
+            raise InvalidTimeException(time)
+        
+        if time < 0 or time > 2359:
+            raise InvalidTimeException(time)
+        
+        return day, time
+    
+    def __parse_period(self, period):
+        if len(period) != 12:
+            raise InvalidPeriodException(period)
+        
+        day = period[0:3]
+        if day not in self._day_names:
+            raise InvalidDayException(day)
+    
+        time_period = period[3:]
+        if '-' not in time_period or len(time_period) != 9:
+            raise InvalidTimePeriodException(time_period)
+        
+        start_time, end_time = time_period.split('-')
+        
+        try:
+            start_time = int(start_time)
+        except (ValueError,):
+            raise InvalidTimeException(start_time)
+        
+        if start_time < 0 or start_time > 2359:
+            raise InvalidTimeException(start_time)
+        
+        try:
+            end_time = int(end_time)
+        except (ValueError,):
+            raise InvalidTimeException(end_time)
+        
+        if end_time < 0 or end_time > 2359:
+            raise InvalidTimeException(end_time)        
+        
+        return day, start_time, end_time
+  
+    def __add__(self, other):
+        new_object = TimeOfWeek()
+        new_object._periods = {}
+        for day in other._periods:
+            start_time, end_time = other._periods[day]
+            if day in self._periods:
+                if self._periods[day][0] < start_time:
+                    start_time = self._periods[day][0]
+                    
+                if self._periods[day][1] > end_time:
+                    end_time = self._periods[day][1]
+                    
+            new_object._periods[day] = (start_time, end_time)
+            
+        for day in self._periods:
+            if day not in new_object._periods:
+                new_object._periods[day] = self._periods[day]
+                    
+        return new_object
+    
+    def __str__(self):
+        outputs = []
+        for day_name in self._day_names:
+            if day_name in self._periods:
+                output = day_name + ''
+                start_time, end_time = self._periods[day_name]
+                output += '%04d-%04d' % (start_time, end_time)
+
+                outputs.append(output)
+                
+        return ', '.join(outputs)
+            
+        
                 
             
